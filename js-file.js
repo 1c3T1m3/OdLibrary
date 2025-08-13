8af83db38c58f24ff8e58346b34996177f88b05c
1c3T1m3 <200052183+1c3T1m3@users.noreply.github.com>
2025-08-13 17:37:29 +0300

    The constructors work now!

--- js-file.js
+++ js-file.js
@@ -2,11 +2,25 @@ console.log("yo!");
 
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, nOfPages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.title = title;
+  this.nOfPages = nOfPages;
+  this.author = author;
+  this.read = read;
+  info = (this.title, this.author, this.nOfPages, "pages", this.read)
+/*  this.sayName = function() {
+    console.log(this.title, this.author, this.nOfPages, "pages", this.read)
+  };*/
+  return info;
 }
 
 function addBookToLibrary() {
   // take params, create a book then store it in the array
   crypto.randomUUID()
 }
+
+NiggaLikeMe = new Book("nigger","nog","nogger","Oh, yes sir!")
+console.log(NiggaLikeMe);
